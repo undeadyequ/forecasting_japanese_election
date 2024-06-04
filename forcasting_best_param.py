89c4c313a329092bc2cc12614f9eb45107e748fe
luoxuan <rosengaga@gmail.com>
2024-06-04 15:54:10 +0900

    add modification

--- forcasting_best_param.py
+++ forcasting_best_param.py
@@ -19,7 +19,6 @@ pd.options.display.float_format = '{:.2f}'.format
 
 def random_select_data(data, s, data_condition=("N2012")):
     csvdata = pd.read_csv(data, delimiter=",")
-
     if "U2009" in data_condition:
         csvdata = csvdata[csvdata['Year'] < 2010]
     if "N2012" in data_condition :
@@ -57,150 +56,110 @@ def train_clf(method, seeds_num, title, csv_data, data_condition):
     x_tests = []
     y_tests = []
 
+    # choose classifier
+    if method == "lr":
+        besthyper = {'fit_intercept': True, 'normalize': False}
+        clf = LinearRegression(**besthyper)
+
+    elif method == "dt":
+        besthyper = {'splitter': 'random', 'random_state': 0, 'max_depth': 3, 'criterion': 'mse'}
+        clf = DecisionTreeRegressor(**besthyper)
+
+    elif method == "svr":
+        best_hyper = {'kernel': 'poly'}
+        clf = SVR(**best_hyper)
+
+    elif method == "rf":
+        best_hyper = {
+            'criterion': 'mae',
+            'max_depth': 4,
+            'max_features': 'auto',
+            'min_samples_leaf': 2,
+            'min_samples_split': 2,
+            'n_estimators': 125,
+            'n_jobs': 1,
+            'random_state': 0}
+        clf = RandomForestRegressor(**best_hyper)
+
+    elif method == "bag_lr":
+        best_hyper = {
+            'base_estimator': LinearRegression(),
+            'bootstrap': True,
+            'max_features': 3,
+            'max_samples': 12,
+            'n_estimators': 2000,
+            'n_jobs': 1,
+            'random_state': 0}
+        clf = BaggingRegressor(**best_hyper)
+
+    elif method == "bag_dt":
+        best_hyper = {
+            'base_estimator': DecisionTreeRegressor(),
+            'bootstrap': True,
+            'max_features': 3,
+            'max_samples': 12,
+            'n_estimators': 2000,
+            'n_jobs': 1,
+            'random_state': 0}
+        clf = BaggingRegressor(**best_hyper)
+
+    elif method == "adaboost_lr":
+        besthyper = {
+            'base_estimator': LinearRegression(),
+            'learning_rate': 1.75,
+            'loss': 'linear',
+            'n_estimators': 10,
+            'random_state': 0
+        }
+        clf = AdaBoostRegressor(**besthyper)
+
+    elif method == "adaboost_dt":
+        besthyper = {
+            'base_estimator': DecisionTreeRegressor(max_depth=4),
+            'learning_rate': 1.75,
+            'loss': 'linear',
+            'n_estimators': 10,
+            'random_state': 0
+        }
+        clf = AdaBoostRegressor(**besthyper)
+
+    elif method == "gradient_boost":
+        besthyper = {
+            'criterion': 'mae',
+            'learning_rate': 0.2,
+            'loss': 'lad',
+            'max_depth': 3,
+            'max_features': 'auto',
+            'min_samples_leaf': 3,
+            'min_samples_split': 2,
+            'n_estimators': 125,
+            'random_state': 0
+        }
+        clf = GradientBoostingRegressor(**besthyper)
+
+    elif method == "xgb":
+        besthyper = {
+            'booster': 'dart',
+            'eta': 0.05,
+            'eval_metric': 'mae',
+            'gamma': 0,
+            'lambda': 0.5,
+            'max_depth': 5,
+            'max_leaf_nodes': 1,
+            'n_estimators': 100,
+            'n_jobs': 1,
+            'random_state': 0,
+            'seed': 0,
+            'verbosity': 0}
+        clf = XGBRegressor(**besthyper)
+    else:
+        clf = None
+        print("Undefined method")
+
     for s in range(2, seeds_num+2):
         x_train, y_train, x_test, y_test = random_select_data(csv_data, s, data_condition)
-        cvnum = x_train.shape[0]
-        myscoring = "neg_mean_absolute_error"
-
-        if method == "lr":
-            besthyper = {'fit_intercept': True, 'normalize': False}
-            clf = LinearRegression(**besthyper)
-
-        elif method == "dt":
-            besthyper = {'splitter': 'random', 'random_state': 0, 'max_depth': 3, 'criterion': 'mse'}
-            clf = DecisionTreeRegressor(**besthyper)
-
-        elif method == "svr":
-            best_hyper = {'kernel': 'poly'}
-            clf = SVR(**best_hyper)
-
-        elif method == "rf":
-            best_hyper = {
-                'criterion': 'mae',
-                'max_depth': 4,
-                'max_features': 'auto',
-                'min_samples_leaf': 2,
-                'min_samples_split': 2,
-                'n_estimators': 125,
-                'n_jobs': 1,
-                'random_state': 0}
-            clf = RandomForestRegressor(**best_hyper)
-
-        elif method == "bag_lr":
-            best_hyper = {
-                'base_estimator': LinearRegression(),
-                'bootstrap': True,
-                'max_features': 3,
-                'max_samples': 12,
-                'n_estimators': 2000,
-                'n_jobs': 1,
-                'random_state': 0}
-            clf = BaggingRegressor(**best_hyper)
-
-        elif method == "bag_dt":
-            best_hyper = {
-                'base_estimator': DecisionTreeRegressor(),
-                'bootstrap': True,
-                'max_features': 3,
-                'max_samples': 12,
-                'n_estimators': 2000,
-                'n_jobs': 1,
-                'random_state': 0}
-            clf = BaggingRegressor(**best_hyper)
-
-        elif method == "adaboost_lr":
-            besthyper = {
-                'base_estimator': LinearRegression(),
-                'learning_rate': 1.75,
-                'loss': 'linear',
-                'n_estimators': 10,
-                'random_state': 0
-            }
-            clf = AdaBoostRegressor(**besthyper)
-
-        elif method == "adaboost_dt":
-            besthyper = {
-                'base_estimator': DecisionTreeRegressor(max_depth=4),
-                'learning_rate': 1.75,
-                'loss': 'linear',
-                'n_estimators': 10,
-                'random_state': 0
-            }
-            clf = AdaBoostRegressor(**besthyper)
-
-        elif method == "gradient_boost":
-            besthyper = {'random_state': 0,
-                         'loss': "ls",
-                         'n_estimators': 40,
-                         'min_samples_split': 3,
-                         'min_samples_leaf': 2,
-                         'max_features': 'auto',
-                         'learning_rate': 0.3,
-                         'max_depth': 5,
-                         'criterion': 'friedman_mse'}
-            besthyper = {
-                'criterion': 'mae',
-                'learning_rate': 0.2,
-                'loss': 'lad',
-                'max_depth': 3,
-                'max_features': 'auto',
-                'min_samples_leaf': 3,
-                'min_samples_split': 2,
-                'n_estimators': 125,
-                'random_state': 0}
-            clf = GradientBoostingRegressor(**besthyper)
-
-        elif method == "gradient_boost":
-            besthyper = {
-                'criterion': 'mae',
-                'learning_rate': 0.2,
-                'loss': 'lad',
-                'max_depth': 3,
-                'max_features': 'auto',
-                'min_samples_leaf': 3,
-                'min_samples_split': 2,
-                'n_estimators': 125,
-                'random_state': 0}
-            clf = GradientBoostingRegressor(**besthyper)
-
-        elif method == "xgb":
-            besthyper = {
-                'booster': 'dart',
-                'eta': 0.05,
-                'eval_metric': 'mae',
-                'gamma': 0,
-                'lambda': 0.5,
-                'max_depth': 5,
-                'max_leaf_nodes': 1,
-                'n_estimators': 100,
-                'n_jobs': 1,
-                'random_state': 0,
-                'seed': 0,
-                'verbosity': 0}
-            clf = XGBRegressor(**besthyper)
-
-        elif method == "xgb":
-            besthyper = {
-                'booster': 'dart',
-                'eta': 0.05,
-                'eval_metric': 'mae',
-                'gamma': 0,
-                'lambda': 0.5,
-                'max_depth': 5,
-                'max_leaf_nodes': 1,
-                'n_estimators': 100,
-                'n_jobs': 1,
-                'random_state': 0,
-                'seed': 0,
-                'verbosity': 0}
-            clf = XGBRegressor(**besthyper)
-
-        else:
-            clf = None
-            print("Undefined clf")
 
         clf.fit(x_train, y_train)
-
         # Evaluation of train and test
         y_train_pred = clf.predict(x_train)
         y_train_pred_index = x_train.index
@@ -239,7 +198,7 @@ def train_clf(method, seeds_num, title, csv_data, data_condition):
     champ_estimator = estimators[champion_number]
 
     # find champion prediction
-    y_pred_champ = [round(x) for x in y_preds[champion_number]]
+    y_pred_champ = [round(x, 2) for x in y_preds[champion_number]]
     y_pred_index_y_pred_champ = y_preds_index[champion_number]
     y_pred_champ = pd.DataFrame(y_pred_champ, index=y_pred_index_y_pred_champ)
 
@@ -249,7 +208,6 @@ def train_clf(method, seeds_num, title, csv_data, data_condition):
         feature_importance = champ_estimator.feature_importances_
         feature_importances.append(feature_importance)
         logging.info("ft importance: {}".format(feature_importances))
-
     #logging.info("average MAE of train(aver of {} shuffles): {}".format(seeds_num, np.mean(train_maes)))
     #logging.info("variacne MAE of train: {}".format(np.std(train_maes)))
     #logging.info("test result:{}".format(test_maes))
@@ -309,42 +267,39 @@ if __name__ == '__main__':
     parser.add_argument("--seed_num", type=int, default=100)
     parser.add_argument("--title", type=str, default="data_election_2020_correct2012")
     parser.add_argument("--resname", type=str, default="method_blr")
-
-    csvdata = pd.read_csv("data_election_pred.csv", delimiter=",")
-
     args = parser.parse_args()
+
     logging.basicConfig(filename=args.log, filemode="a", format="%(asctime)s - %(message)s", level=logging.INFO)
-    #csv_data = "data_election.csv"
-    #csv_data = "data_election_2020.csv"
-    #csv_data = "data_election_correct2012.csv"
+
+    # input
     csv_data = "data_election_2020_correct2012.csv"
+    # Condition
+    #methods = ["lr", "dt", "bg_lr", "bag_dt", "gradient_boost"] # grandient_boost_lr is in other file
+    methods = ["lr", "dt", "bag_lr", "bag_dt", "gradient_boost"]  # grandient_boost_lr is in other file
 
-    res_dir = "exp"
-    vis_subdir = "vis"
+    data_condtion = "N2012"
 
-    #explore_data(csv_data)
-    # explore_data_2d(csv_f)
+    # output
+    ft_importance_f = "result/feature_import.json"
+    pred_f = "result/data_election_pred3.csv"
+    acc_f = "result/method_acc_temp.csv"
 
-    data_condition = ("1960-2017-N2012", "1960-2017-N2012-N2009", "1960-2021-N2012")
-    # Condition: method, data
-    # method:
+
+    csvdata = pd.read_csv(csv_data, delimiter=",")
+    if "U2009" in data_condtion:
+        csvdata = csvdata[csvdata['Year'] < 2010]
+    if "N2012" in data_condtion :
+        csvdata = csvdata[csvdata['Year'] != 2012]
+    if "N2009" in data_condtion:
+        csvdata = csvdata[csvdata['Year'] != 2009]
 
     result = []
-    methods = ["lr", "dt", "rf", "bag", "adaboost", "gradient_boost"]
-    methods_wl = ["lr", "dt", "rf", "bag_lr", "bag_dt", "adaboost_lr", "adaboost_dt", "gradient_boost"]
-    methods_xgb = ["lr", "dt", "rf", "bag", "adaboost", "xgb", "gradient_boost"]
-    methods_blr = ["blr"]
-
-
     feats_arrays = {}
     for method in methods:
-        #logging.info("***** Start Train by Method: {} with {} *****".format(method, csv_data))
-        #logging.info("Config: {}".format(""))
-
         st = time.time()
         train_maes, train_rmses, test_maes, test_rmses, y_pred_champ, \
         feats_important_dicts, feats_import_array = \
-            train_clf(method, args.seed_num, args.title, csv_data, data_condition=("N2012"))
+            train_clf(method, args.seed_num, args.title, csv_data, data_condition=(data_condtion)) # "1960-2017-N2012", "1960-2017-N2012-N2009", "1960-2021-N2012"
         end = time.time()
         cls_res = [np.mean(train_maes), np.std(train_maes),
                    np.mean(train_rmses), np.std(train_rmses),
@@ -359,22 +314,12 @@ if __name__ == '__main__':
                    ]
         result.append(cls_res)
         print("{} time cost :{}".format(method, end-st))
-        #logging.info("{} time cost :{}".format(method, (end-st)/3600))
-
         # add prediction result
         csvdata[method] = y_pred_champ
         feats_arrays[method] = feats_import_array.tolist()
 
-    # accuracy csv
-    with open("res_save.csv", "w") as f:
-        for l in result:
-            f.write(str(l) + "\n")
-
-    with open("result/feature_import.json", "w") as f:
-        json.dump(feats_arrays, f)
-
     pd_data = pd.DataFrame(np.array(result),
-                           index=methods_wl,
+                           index=methods,
                            columns=["train_mae", "train_mae_var",
                                     "train_rmse", "train_rmse_var",
                                     "test_mae", "test_mae_var",
@@ -383,7 +328,29 @@ if __name__ == '__main__':
                                     "imp_GDP_mean", "imp_GDP_std",
                                     "imp_approval_mean", "imp_approval_std"
                                     ])
-    print(pd_data)
+    # OUTPUT
+    # accuracy and importance score csv
+    pd_data.to_csv("result/method_acc_v1.csv")
     # pred_data and acc_data
-    csvdata.to_csv(args.methods + "_pred.csv")
-    pd_data.to_csv(args.methods + "_acc1.csv")
+    csvdata.to_csv("result/data_election_pred4.csv")
+
+    # Other output
+    if False:
+        with open("res_save.csv", "w") as f:
+            for l in result:
+                f.write(str(l) + "\n")
+        with open("result/feature_import.json", "w") as f:
+            json.dump(feats_arrays, f)
+
+
+    """
+    #methods = ["lr", "dt", "rf", "bag", "adaboost", "gradient_boost"]
+    methods_wl = ["lr", "dt", "rf", "bag_lr", "bag_dt", "adaboost_lr", "adaboost_dt", "gradient_boost"]
+    methods_xgb = ["lr", "dt", "rf", "bag", "adaboost", "xgb", "gradient_boost"]
+    methods_blr = ["blr"]
+    #methods = ["lr", "gradient_boost"]
+    
+
+
+
+    """
